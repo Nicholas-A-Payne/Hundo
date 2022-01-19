a5fbe89e8134ea44e862b6392f67760376e77acb
Nicholas Payne <94993634+NFSnicholas@users.noreply.github.com>
2022-01-19 11:34:34 -0500

    Initial commit

--- JS/site.js
+++ JS/site.js
@@ -1,14 +1,62 @@
 
-// Dispalying Darren to unsuspecting Victims
-function getMessage(){
-
-    let userMessage = document.getElementById("message").value;
+//Program Entry Point
+function getValues2() {
     
-Swal.fire(
-    'The App Title',
-    userMessage,
-    'question'
-    );
+    let startValue = document.getElementById("startValue").value;
+    let endValue = document.getElementById("endValue").value;
 
-    console.log(userMessage);
+    startValue = parseInt(startValue);
+    endValue = parseInt (endValue);
+   
+    //check to see if they are IN FACT an integer
+    if (Number.isInteger(startValue) && Number.isInteger(endValue)) {
+
+        //generate a list of numbers
+        let numbers = generateNumbers(startValue, endValue);
+        
+        //display a list of numbers
+        displayNumbers(numbers);
+
+    }
+    else {
+        Swal.fire({
+             icon: 'error',
+             title: 'oops...',
+             text: 'Only integers are allowed for Hundo!'
+        }
+        )
+    }
+}
+
+function generateNumbers(start, end){
+
+        let numbers= [];
+        for (let index = start; index <= end; index++){
+            numbers.push(index);
+        }
+
+        return numbers;
+
+}
+
+function displayNumbers(numbers){
+
+    let templateRows = "";
+    let className = "";
+    for (let index = 0; index < numbers.length; index++){
+
+        let number = numbers[index];
+        
+        if (number % 2 == 0 ){
+            className = "even";
+        }
+        else{
+            className = "odd";
+        }
+
+        let row =  `<tr><td class="${className}">${number}</td></tr>`;
+        templateRows += row;
+    }
+
+    document.getElementById("results").innerHTML = templateRows;
 }
